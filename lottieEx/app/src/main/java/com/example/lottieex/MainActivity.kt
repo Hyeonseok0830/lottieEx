06445318e15c34055de2144309873834aba1b685
Hyeonseok <56476096+Hyeonseok0830@users.noreply.github.com>
2021-03-28 23:02:58 +0900

    heart 애니메이션 추가 프레임 레이아웃에 동적으로 애니메이션 추가

--- lottieEx/app/src/main/java/com/example/lottieex/MainActivity.kt
+++ lottieEx/app/src/main/java/com/example/lottieex/MainActivity.kt
@@ -1,14 +1,44 @@
 package com.example.lottieex
 
+import android.animation.Animator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import com.airbnb.lottie.LottieAnimationView
+import com.airbnb.lottie.LottieDrawable
 import com.example.lottieex.databinding.ActivityMainBinding
+import android.animation.Animator.AnimatorListener as AnimatorListener1
 
 class MainActivity : AppCompatActivity() {
     val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
+        val frame = binding.animFrame
+        binding.click.setOnClickListener {
+            val animationView = LottieAnimationView(this)
+            animationView.setAnimation("heart.json")
+            animationView.playAnimation()
+            //val params = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT , ViewGroup.LayoutParams.WRAP_CONTENT)
+            //animationView.layoutParams = params
+            frame.addView(animationView, ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT , ViewGroup.LayoutParams.WRAP_CONTENT))
+            animationView.addAnimatorListener(object : Animator.AnimatorListener{
+                override fun onAnimationRepeat(animation: Animator?) {
+                }
+                override fun onAnimationEnd(animation: Animator?) {
+                   frame.removeViewAt(0)
+                }
+                override fun onAnimationCancel(animation: Animator?) {
+                }
+                override fun onAnimationStart(animation: Animator?) {
+                }
+            })
+        }
     }
 }
+
+
